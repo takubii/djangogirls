40d267ce761afd20d53462c4aa6782ec829937af
takubii <vicsat93@gmail.com>
2023-09-27 11:20:55 +0900

    add: コメント投稿機能を追加

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("post/<int:pk>/edit/", views.post_edit, name="post_edit"),
     path("post/<int:pk>/publish/", views.post_publish, name="post_publish"),
     path("post/<int:pk>/remove/", views.post_remove, name="post_remove"),
+    path("post/<int:pk>/comment/", views.add_comment_to_post, name="add_comment_to_post"),
     path("post/new/", views.post_new, name="post_new"),
     path("drafts/", views.post_draft_list, name="post_draft_list"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,12 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 
-from .forms import PostForm
+from .forms import CommentForm, PostForm
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by(
-        "published_date"
-    )
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
     return render(request, "blog/post_list.html", {"posts": posts})
 
 
@@ -65,3 +63,17 @@ def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect("post_list")
+
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect("post_detail", pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, "blog/add_comment_to_post.html", {"form": form})
